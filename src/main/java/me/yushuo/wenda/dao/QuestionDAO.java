e6c81e608baa97abbd4aac8ccc03ed6239171f72
yussuy <secretys123@gmail.com>
2018-12-07 01:43:49 +0800

    首页的后台部分

--- src/main/java/me/yushuo/wenda/dao/QuestionDAO.java
+++ src/main/java/me/yushuo/wenda/dao/QuestionDAO.java
@@ -18,10 +18,10 @@ public interface QuestionDAO {
             ") values (#{title},#{content},#{createdDate},#{userId},#{commentCount})"})
     int addQuestion(Question question);
 
-//    List<Question> selectLatestQuestions(@Param("userId") int userId, @Param("offset") int offset,
-//                                         @Param("limit") int limit);
+    List<Question> selectLatestQuestions(@Param("userId") int userId, @Param("offset") int offset,
+                                         @Param("limit") int limit);
 
-    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, " where user_id = #{userId} order by id desc limit #{offset}, #{limit}"})
-    List<Question> selectLatestQuestions(int userId, int offset, int limit);
+//    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, " where user_id = #{userId} order by id desc limit #{offset}, #{limit}"})
+//    List<Question> selectLatestQuestions(int userId, int offset, int limit);
 
 }
--- src/test/java/me/yushuo/wenda/InitDatabaseTests.java
+++ src/test/java/me/yushuo/wenda/InitDatabaseTests.java
@@ -54,5 +54,7 @@ public class InitDatabaseTests {
         Assert.assertEquals("newpassword", userDAO.selectById(1).getPassword());
         userDAO.deleteById(1);
         Assert.assertNull(userDAO.selectById(1));
+
+        System.out.println(questionDAO.selectLatestQuestions(0, 0, 10));
     }
 }
