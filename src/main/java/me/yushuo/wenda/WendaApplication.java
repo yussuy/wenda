5571dc93ae7c91d05bfb264418a28f822ca411c0
ys <secretys123@gmail.com>
2019-07-26 02:23:26 +0800

    配置入口类

--- src/main/java/me/yushuo/wenda/WendaApplication.java
+++ src/main/java/me/yushuo/wenda/WendaApplication.java
@@ -2,11 +2,17 @@ package me.yushuo.wenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@MapperScan("me.yushuo.wenda.dao")
 public class WendaApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WendaApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WendaApplication.class, args);
 	}
